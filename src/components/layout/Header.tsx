2d09d1f003bedac83147f6eb59efd0185c3c0788
Quentin Watt <quentin@qcasts.com>
2024-03-08 15:48:24 +0200

    Link the logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,9 @@ const Header: React.FC = () => {
     <header className="border-b bg-white">
       <nav className="container mx-auto px-3">
         <div className="flex justify-between items-center h-16">
-          <span className="font-bold text-3xl">Qcasts</span>
+          <span className="font-bold text-3xl">
+            <Link to="/">Qcasts</Link>
+          </span>
           <ul className="flex items-center">
             <li className="mr-2">
               <Link to="/">Home</Link>
