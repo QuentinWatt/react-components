9a5c2be3b18b59bc40365607492d5df18e75dd68
Quentin Watt <quentin@qcasts.com>
2024-03-09 14:18:33 +0200

    Get jest tests working

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  rootDir: 'src',
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    '^@app/(.*)$': '<rootDir>/$1',
+  },
+};
